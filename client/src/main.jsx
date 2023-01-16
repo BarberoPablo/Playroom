c9082180f2cf06bc49da8489703891d6a957908b
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-16 19:43:30 -0300

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
-import App from "./components/app/App";
-import Chat from "./components/Chat/Chat";
+import Home from "./components/Home/Home.jsx";
+import App from "./components/app/App.jsx";
+import Chat from "./components/Chat/Chat.jsx";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
