95e7378e9df69a1bba9342c325892d9a249013ff
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-16 12:03:19 -0300

    Vercel settup

--- server/index.js
+++ server/index.js
@@ -1,19 +1,10 @@
 const express = require("express");
+const { Server: SocketServer } = require("socket.io");
 const morgan = require("morgan");
 const cors = require("cors");
-const { Server: SocketServer } = require("socket.io");
 const http = require("http");
 const configuration = require("./config.js");
-const mongoose = require("mongoose");
-const CONNECTION_URL = `mongodb+srv://admin:aDqvIJdJh6GNfiBD@cluster0.dycq5dv.mongodb.net/test`;
 const PORT = 3001;
-mongoose
-  .set("strictQuery", true)
-  .connect(CONNECTION_URL)
-  .then(() => console.log(`Server running on port ${PORT}`))
-  .catch((error) => {
-    console.log(error.message);
-  });
 
 //  Convierto la aplicacion de express en un servidor http y lo recibe el wss
 const app = express();
@@ -21,7 +12,7 @@ const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
     /* origin: "http://localhost:5173" */
-    origin: "*",
+    origin: "*", //CAMBIAR ESTO A LA URL DE VERCEL
   },
 });
 
