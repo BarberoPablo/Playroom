5dc64c540eae4b91bcf1dab838790902268bc762
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-17 11:44:34 -0300

    deploy

--- client/src/configuration.js
+++ client/src/configuration.js
@@ -2,8 +2,7 @@ import io from "socket.io-client";
 
 export const configuration = {
   //WSSURL: "http://localhost:3001",
-  //WSSURL: "https://playroom-production.up.railway.app/",
-  WSSURL: "https://playroom-psi.vercel.app/",
+  WSSURL: "https://playroom-production.up.railway.app/",
 };
 
 export const socket = io(configuration.WSSURL);
