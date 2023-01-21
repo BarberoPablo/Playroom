a87483b84d4c90c06f6d14efd1b4d3352f1df99d
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-21 20:41:25 -0300

    feat: array of avatar images

---
+++ client/src/assets/Avatars/avatars.js
@@ -0,0 +1,7 @@
+import one from "./1.png";
+import two from "./2.png";
+import three from "./3.png";
+import four from "./4.png";
+import five from "./5.png";
+
+export const avatars = [one, two, three, four, five];
