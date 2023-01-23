b9e11dfdf6e1f2e2851e57935de27dd2b50246a3
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-23 20:32:49 -0300

    Add avatars

--- client/src/assets/Avatars/avatars.js
+++ client/src/assets/Avatars/avatars.js
@@ -4,5 +4,9 @@ import three from "./3.png";
 import four from "./4.png";
 import five from "./5.png";
 import six from "./6.png";
+import seven from "./7.png";
+import eight from "./8.png";
+import nine from "./9.png";
+import ten from "./10.png";
 
-export const avatars = [one, two, three, four, five, six];
+export const avatars = [one, two, three, four, five, six, seven, eight, nine, ten];
