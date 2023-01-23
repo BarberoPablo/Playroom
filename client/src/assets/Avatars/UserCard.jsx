78fc142ad2ca9fb4c1374a7d3b62b031c4b52dc8
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-23 01:01:46 -0300

    Styles

--- client/src/assets/Avatars/UserCard.jsx
+++ client/src/assets/Avatars/UserCard.jsx
@@ -6,24 +6,31 @@ import "./UserCard.css";
 const UserCard = ({ user, size, me, click, playing }) => {
   const cardFormat = () => {
     if (me) {
-      return <Avatar size={size} src={avatars[user.avatar]} />;
+      return <Avatar size={size} src={avatars[user.avatar]} style={{ borderRadius: "0px" }} />;
     }
 
     if (playing) {
       return (
         <Tooltip title="User is already playing">
-          <Avatar size={size} src={avatars[user.avatar]} style={{ filter: "grayscale(100%)" }} />
+          <Avatar
+            className="avatar-image"
+            size={size}
+            src={avatars[user.avatar]}
+            style={{ filter: "grayscale(100%)" }}
+          />
         </Tooltip>
       );
     } else {
       return (
         <div className="avatar-conteiner">
-          <Avatar
-            className="avatar-image"
-            size={size}
-            src={avatars[user.avatar]}
-            onClick={(e) => click(e, user)}
-          />
+          <Tooltip title="Challenge this user!">
+            <Avatar
+              className="avatar-image"
+              size={size}
+              src={avatars[user.avatar]}
+              onClick={(e) => click(e, user)}
+            />
+          </Tooltip>
           <h2 className="avatar-username" onClick={(e) => click(e, user)}>
             {user.username}
           </h2>
